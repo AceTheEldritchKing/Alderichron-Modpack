ef88c36c7ecb745695c244740ab8528fa4cb0d39
Davey <149535160+meeprdib@users.noreply.github.com>
2024-04-21 23:00:38 -0400

    Removed a lot of thematically inappropriate items (Create mod)
    might go back in here and organize shit better idk

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -9,6 +9,29 @@ ServerEvents.recipes(event => {
     // From The Shadows
     event.remove({output: 'fromtheshadows:thirst_for_blood'})
 
+    // Create
+
+    event.remove({output:
+    'create:potato_cannon',
+    'create:steam_engine',
+    'create:steam_whistle',
+    'create:controller_rail',
+    'create:cart_assembler',
+    'create:mechanical_roller',
+    'create:mechanical_drill',
+    'create:flywheel',
+    'create:track',
+    'create:railway_casing',
+    'create:schedule',
+    'create:track_station',
+    'create:track_signal',
+    'create:track_observer',
+    'create:controls',
+    'create:display_link',
+    'create:display_board',
+    'create:nixie_tube'
+    })
+
     // Alex's Mobs
     event.remove({ output: 'alexsmobs:hemolymph_blaster'})
     event.remove({ output: 'alexsmobs:blood_sprayer'})
@@ -17,3 +40,5 @@ ServerEvents.recipes(event => {
 
     console.log('Heya, recipes!')
 })
+
+//hai ace :3
